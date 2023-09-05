5a73e8467df7cff61c26b0d168a99d1dc42315cb
Robert Mikolajczyk <ronert1674@gmail.com>
2023-09-05 12:52:21 +0200

    removes duplication by means of caesar function

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
-def encrypt(text_to_encrypt: str, offset: int):
+def caesar(text: str, offset: int, direction: str):
     alphabet = ["a","b","c","d","e","f","g","h","i","j","k",
                 "l","m","n","o","p","q","r","s","t","u","v",
                 "w","x","y","z"," "]
 
-    encrypted_text = ""
+    caesar_text = ""
 
-    for letter in text_to_encrypt:
-        encrypted_text += alphabet[(alphabet.index(letter) + offset) % 27]
+    if direction == "encode":
+        for letter in text:
+            caesar_text += alphabet[(alphabet.index(letter) + offset) % 27]
+    elif direction == "decode":
+        for letter in text:
+            caesar_text += alphabet[(alphabet.index(letter) - offset) % 27]
 
-    return encrypted_text
-
-
-def decrypt(text_to_decrypt: int, offset: int):
-    alphabet = ["a","b","c","d","e","f","g","h","i","j","k",
-                "l","m","n","o","p","q","r","s","t","u","v",
-                "w","x","y","z"," "]
-
-    decrypted_text = ""
-
-    for letter in text_to_decrypt:
-        decrypted_text += alphabet[(alphabet.index(letter) - offset) % 27]
-
-    return decrypted_text
+    return caesar_text
 
 
 def main():
@@ -29,12 +20,7 @@ def main():
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
-    if direction == "encode":
-        print(encrypt(text_to_encrypt=text, offset=shift))
-    elif direction == "decode":
-        print(decrypt(text_to_decrypt=text, offset=shift))
-    else:
-        print("Incorect type of action!")
+    print(caesar(text=text, offset=shift, direction=direction))
 
 
 if __name__ == "__main__":
