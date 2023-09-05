f77f839a44cd27bf8399f4e8b024b3e452fc7fa5
Robert Mikolajczyk <ronert1674@gmail.com>
2023-09-05 19:41:51 +0200

    improves user experience

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
+from caesar_art import logo
+import os
+
 def caesar(text: str, offset: int, direction: str):
     alphabet = ["a","b","c","d","e","f","g","h","i","j","k",
                 "l","m","n","o","p","q","r","s","t","u","v",
-                "w","x","y","z"," "]
+                "w","x","y","z"]
 
     caesar_text = ""
 
-    if direction == "encode":
-        for letter in text:
-            caesar_text += alphabet[(alphabet.index(letter) + offset) % 27]
-    elif direction == "decode":
-        for letter in text:
-            caesar_text += alphabet[(alphabet.index(letter) - offset) % 27]
+    for letter in text:
+        if letter in alphabet:
+            if direction == "encode":
+                caesar_text += alphabet[(alphabet.index(letter) + offset) % 26]
+            elif direction == "decode":
+                caesar_text += alphabet[(alphabet.index(letter) - offset) % 26]
+        else:
+                caesar_text += letter
 
     return caesar_text
 
 
 def main():
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
+    os.system('clear')
+    print(logo)
+    should_stop = False
+    while not is_stopped:
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
 
-    print(caesar(text=text, offset=shift, direction=direction))
+        print(caesar(text=text, offset=shift, direction=direction))
+        answer = input("Type 'no' if you want to stop, otherwise type any key.").lower()
+        if answer == 'no':
+            should_stop = True
+            print("Goodbye")
+
 
 
 if __name__ == "__main__":
